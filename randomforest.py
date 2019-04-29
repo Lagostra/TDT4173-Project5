0bf599eef8f8291aa2b5dde005131eb0291af13c
Eivind Lie Andreassen <ela4991@gmail.com>
2019-04-29 11:41:16 +0200

    Exchange pickle for joblib to support massive models

--- randomforest.py
+++ randomforest.py
@@ -1,5 +1,5 @@
 import numpy as np
-import pickle
+import joblib
 
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
@@ -8,12 +8,12 @@ from sklearn.model_selection import train_test_split
 
 def save(model, path):
     with open(path, 'wb') as f:
-        pickle.dump(model, f)
+        joblib.dump(model, f)
 
 
 def load(path):
     with open(path, 'rb') as f:
-        return pickle.load(f)
+        return joblib.load(f)
 
 
 def evaluate(model, x):
@@ -30,7 +30,7 @@ if __name__ == '__main__':
 
     train_x, test_x, train_y, test_y = train_test_split(x, y, random_state=42)
 
-    model = RandomForestClassifier(n_estimators=2000, max_features=50, n_jobs=4, verbose=True)
+    model = RandomForestClassifier(n_estimators=1000, max_features=40, n_jobs=4, verbose=True)
     model.fit(train_x, train_y)
 
     save(model, 'model/randomforest')
